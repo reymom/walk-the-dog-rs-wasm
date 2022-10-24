9b5bd96c9d7713c7791f718161b8be3141dc2525
RamonGarcia <ramon.seuma@vanillasteel.com>
2022-10-24 18:26:24 +0200

    test frame rates

--- src/engine/game.rs
+++ src/engine/game.rs
@@ -1,6 +1,6 @@
 use crate::browser;
 use crate::engine::keys::{prepare_input, process_input};
-use crate::engine::{GameLoop, KeyState, Renderer};
+use crate::engine::{draw_frame_rate, GameLoop, KeyState, Renderer};
 use crate::game::Game;
 
 use anyhow::{anyhow, Result};
@@ -30,7 +30,8 @@ impl GameLoop {
         let mut keystate = KeyState::new();
         *g.borrow_mut() = Some(browser::create_raf_closure(move |perf: f64| {
             process_input(&mut keystate, &mut keyevent_receiver);
-            game_loop.accumulated_delta += (perf - game_loop.last_frame) as f32;
+            let frame_time = perf - game_loop.last_frame;
+            game_loop.accumulated_delta += frame_time as f32;
             while game_loop.accumulated_delta > FRAME_SIZE {
                 game.update(&keystate);
                 game_loop.accumulated_delta -= FRAME_SIZE;
@@ -39,7 +40,13 @@ impl GameLoop {
 
             game.draw(&renderer);
 
-            browser::request_animation_frame(f.borrow().as_ref().unwrap());
+            if cfg!(debug_assertions) {
+                unsafe { draw_frame_rate(&renderer, frame_time) }
+            }
+
+            if let Err(err) = browser::request_animation_frame(f.borrow().as_ref().unwrap()) {
+                panic!("Error while requesting animation frame {:#?}", err);
+            }
         }));
         browser::request_animation_frame(
             g.borrow()
